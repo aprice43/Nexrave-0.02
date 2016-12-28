0d339b9b096eb9b78c581ebbd292b1baa23dcbb4
aprice43 <AlvanPrice@alvans-air>
2016-12-28 11:20:04 -0500

    Initial Commit

---
+++ Nexrave 0.02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nexrave 0.02
+//
+//  Created by Alvan Price on 12/28/16.
+//  Copyright © 2016 Nexrave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
