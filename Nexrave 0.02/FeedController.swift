6820f7120e44167bdc060f0f9db9c166bb72e2c8
aprice43 <AlvanPrice@alvans-air>
2016-12-29 18:32:18 -0500

    Timeline basically done with placeholder pictures. Event View has be created but not coded. Menu still need to be added. Firebase functionality need to be done.

--- Nexrave 0.02/FeedController.swift
+++ Nexrave 0.02/FeedController.swift
@@ -15,9 +15,12 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     let customIdentifier = "custom ID"
     override func viewDidLoad(){
     super.viewDidLoad()
-    
-        
-    navigationItem.title = "Nexrave"
+    let titleLogo : UIImage = UIImage(named: "FeedLogo")!
+    let marginX: CGFloat = (self.navigationController!.navigationBar.frame.size.width / 2) - 45
+    let imageView = UIImageView(frame: CGRect(x: marginX, y: 0, width: 90, height: 45))
+    imageView.contentMode = .scaleAspectFit
+    imageView.image = titleLogo
+    self.navigationController?.navigationBar.addSubview(imageView)
     self.collectionView?.backgroundColor = UIColor.black
     collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customIdentifier)
         
@@ -29,14 +32,21 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     let names = [" 1.png ", "2.JPG", "3.jpeg" , "4.png"]
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return 2
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (view.frame.width), height: (view.frame.width + 30))
     }
+    func transitionButtonPressed() {
+     performSegue(withIdentifier: "goToFeed", sender: self)
+        
+    }
+
 
 
 }
+
+
 
 
 class CustomCell: UICollectionViewCell {
@@ -50,23 +60,45 @@ class CustomCell: UICollectionViewCell {
     }
     let hostPicture: UIImageView = {
         let hostView = UIImageView()
+        hostView.image = UIImage(named: "3")
         hostView.contentMode = .scaleAspectFit
         hostView.backgroundColor = UIColor.black
-        hostView.translatesAutoresizingMaskIntoConstraints = false
         return hostView
     }()
     let hostNameLabel: UILabel = {
         let label = UILabel()
         label.text = "Gucci Mane"
         label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
+  
         return label
     }()
     let eventDateLabel: UILabel = {
         let label = UILabel()
         label.text = "1/09/16"
         label.font = UIFont.boldSystemFont(ofSize: 30)
-        label.translatesAutoresizingMaskIntoConstraints = false
+     
+        return label
+    }()
+    
+    let flyerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "waka")
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    let transitionButton: UIButton = {
+        let button = UIButton()
+        button.setImage( #imageLiteral(resourceName: "redButton"), for: .normal)
+        button.addTarget(self, action: #selector(FeedController.transitionButtonPressed) , for: .touchUpInside)
+        return button
+        
+    
+    }()
+    let eventTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Waka Party"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 30)
         return label
     }()
 
@@ -78,13 +110,26 @@ class CustomCell: UICollectionViewCell {
         addSubview(hostNameLabel)
         addSubview(hostPicture)
         addSubview(eventDateLabel)
+        addSubview(flyerImageView)
+        addSubview(transitionButton)
+        addSubview(eventTitleLabel)
         
-        addConstriantsWithFormat(format: "H:|-8-[v0(44)]-8-[v1][v3]-25-|", views: hostPicture,hostNameLabel,eventDateLabel)
+        addConstriantsWithFormat(format: "H:|-8-[v0(44)]-8-[v1][v2]-25-|", views: hostPicture, hostNameLabel, eventDateLabel)
+        addConstriantsWithFormat(format: "H:|[v0]|", views: flyerImageView)
+        addConstriantsWithFormat(format: "H:|[v0]|", views: transitionButton)
+        addConstriantsWithFormat(format: "H:|-20-[v0]", views: eventTitleLabel)
         addConstriantsWithFormat(format: "V:|-10-[v0]", views: hostNameLabel)
         addConstriantsWithFormat(format: "V:|-8-[v0(44)]", views: hostPicture)
-        addConstriantsWithFormat(format: "V:|-10-[v0]", views: eventDateLabel)
+        addConstriantsWithFormat(format: "V:|-65-[v0(320)][v1]|", views: flyerImageView, transitionButton)
+        addConstriantsWithFormat(format: "V:|-8-[v0]", views: eventDateLabel)
+        addConstriantsWithFormat(format: "V:[v0]-10-|", views: eventTitleLabel)
+
    
     }
+    
+    
+
+   
 }
 
 extension UIView {
@@ -99,6 +144,8 @@ extension UIView {
         }
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
+
+    
     
     
     
---
+++ Nexrave 0.02/eventpageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  evenpageViewController.swift
+//  Nexrave 0.02
+//
+//  Created by Alvan Price on 12/29/16.
+//  Copyright © 2016 Nexrave. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+
+
+class eventpageViewController: UIViewController {
+
+}
