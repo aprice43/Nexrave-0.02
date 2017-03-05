a835121a87f7e3bb3fe3ff828150490a86685cce
aprice43 <AlvanPrice@lawn-128-61-30-61.lawn.gatech.edu>
2017-03-05 18:03:41 -0500

    Sunday March 5
    Profile Cell and Event Page Work

--- Nexrave 0.02/EventPage.swift
+++ Nexrave 0.02/EventPage.swift
@@ -22,19 +22,86 @@ class EventCell : UICollectionViewCell {
         let flyer = UIImageView()
         flyer.image = UIImage(named: "waka")
         flyer.contentMode = .scaleAspectFill
+		flyer.clipsToBounds = true
+		flyer.alpha = 0.5
         flyer.backgroundColor = UIColor.black
         return flyer
     }()
-    
+	let hostPhoto: UIImageView = {
+		let photo = UIImageView()
+		photo.image = UIImage(named: "3")
+		photo.layer.borderColor = UIColor.white.cgColor
+		photo.layer.borderWidth = 3
+		photo.layer.cornerRadius = 3
+		photo.clipsToBounds = true
+		
+		return photo
+	}()
+	let flyerOverlay : UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor.black
+		view.alpha = 0.3
+		return view
+	}()
+	let dateLabel: UILabel = {
+		let label = UILabel()
+		label.text = " 8:30PM"
+		label.textColor = UIColor.white
+		label.font = UIFont.boldSystemFont(ofSize: 40)
+		return label
+	}()
+	let eventName: UILabel = {
+		let label = UILabel()
+		label.text = "Event Name Here"
+		label.textColor = UIColor.white
+		label.font = UIFont.boldSystemFont(ofSize: 30)
+		return label
+	}()
+	let ticketButton: UIButton = {
+		
+		let button = UIButton()
+		button.backgroundColor = UIColor.red
+		button.tintColor = UIColor.black
+		let ticket = UILabel()
+		ticket.text = "TICKET"
+		ticket.font = UIFont.boldSystemFont(ofSize: 25)
+		ticket.textColor = UIColor.white
+		ticket.textAlignment = .center
+		button.addSubview(ticket)
+		button.addConstriantsWithFormat(format: "H:|[v0]|", views: ticket)
+		button.addConstriantsWithFormat(format: "V:|[v0]|", views: ticket)
+		return button
+		
+	}()
+	let horDivider: UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor.white
+		
+		
+		return view
+	}()
     func setupViews(){
     
         backgroundColor = UIColor.black
         
         addSubview(flyerHeader)
-        
-        
+		addSubview(horDivider)
+        addSubview(hostPhoto)
+		addSubview(dateLabel)
+		addSubview(eventName)
+		addSubview(ticketButton)
+		flyerHeader.addSubview(flyerOverlay)
+		flyerHeader.addConstriantsWithFormat(format: "H:|[v0]|", views: flyerOverlay)
+		flyerHeader.addConstriantsWithFormat(format: "V:|[v0]|", views: flyerOverlay)
+        addConstriantsWithFormat(format: "H:|[v0]|", views: horDivider)
+		addConstriantsWithFormat(format: "H:|-8-[v0(90)]-8-[v1]", views: hostPhoto, eventName)
+		addConstriantsWithFormat(format: "H:|-8-[v0(90)]-8-[v1]-8-|", views: hostPhoto, ticketButton)
+		addConstriantsWithFormat(format: "V:|-180-[v0(90)]", views: hostPhoto)
+		addConstriantsWithFormat(format: "H:[v0]-8-|", views: dateLabel)
+		addConstriantsWithFormat(format: "V:|-20-[v0]", views: dateLabel)
         addConstriantsWithFormat(format: "H:|[v0]|", views: flyerHeader)
-        addConstriantsWithFormat(format: "V:|[v0(150)]", views: flyerHeader)
+        addConstriantsWithFormat(format: "V:|[v0(220)][v1(3)]", views: flyerHeader,horDivider)
+		addConstriantsWithFormat(format: "V:[v0]-3-[v1]-9-[v2]", views: eventName,horDivider,ticketButton)
         
 
     }
--- Nexrave 0.02/ProfilePage.swift
+++ Nexrave 0.02/ProfilePage.swift
@@ -19,24 +19,61 @@ class ProfileCell : UICollectionViewCell {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
-	let flyerHeader: UIImageView = {
+	let ProfileHeader: UIImageView = {
 		let flyer = UIImageView()
-		flyer.image = UIImage(named: "waka")
+		flyer.image = UIImage(named: "thug")
 		flyer.contentMode = .scaleAspectFill
+		flyer.clipsToBounds = true
+		flyer.alpha = 0.8
 		flyer.backgroundColor = UIColor.black
+		
 		return flyer
+	}()
+	let effectView : UIVisualEffectView = {
+		let view = UIVisualEffectView()
+		view.effect = UIBlurEffect(style: (UIBlurEffectStyle(rawValue: 1))!)
+		
+		return view
+		
+		
+	}()
+	let userPhoto: UIImageView = {
+		let photo = UIImageView()
+		photo.image = UIImage(named: "thug")
+		photo.layer.borderColor = UIColor.white.cgColor
+		photo.layer.borderWidth = 2
+		photo.layer.cornerRadius = 75
+		photo.isUserInteractionEnabled = true
+		photo.clipsToBounds = true
+		
+		return photo
+	}()
+	
+	let horDivider: UIView = {
+		let view = UIView()
+		view.backgroundColor = UIColor.white
+		
+		
+		return view
 	}()
 	
 	func setupViews(){
 		
 		backgroundColor = UIColor.black
 		
-		addSubview(flyerHeader)
+		addSubview(ProfileHeader)
+		addSubview(effectView)
+		addSubview(horDivider)
+		effectView.addSubview(userPhoto)
+		addConstriantsWithFormat(format: "H:|[v0]|", views: ProfileHeader)
+		addConstriantsWithFormat(format: "H:|[v0]|", views: horDivider)
+		addConstriantsWithFormat(format: "H:|[v0]|", views: effectView)
+		effectView.addConstriantsWithFormat(format: "H:|-\((UIScreen.main.bounds.width / 2) - 75)-[v0(150)]|", views: userPhoto)
+		effectView.addConstriantsWithFormat(format: "V:|-20-[v0(150)]", views: userPhoto)
+		addConstriantsWithFormat(format: "V:|[v0(250)]", views: effectView)
+		addConstriantsWithFormat(format: "V:|[v0(250)][v1(3)]", views: ProfileHeader, horDivider)
 		
-		
-		addConstriantsWithFormat(format: "H:|[v0]|", views: flyerHeader)
-		addConstriantsWithFormat(format: "V:|[v0(150)]", views: flyerHeader)
-		
+
 		
 	}
 	
